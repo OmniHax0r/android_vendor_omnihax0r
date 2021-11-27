e5a5eed59ff48892bc10cf26fb4c24465d34203e
Geofferey Eakins <geofferey.eakins@gmail.com>
2021-11-26 21:16:07 -0800

    add: bash prebuilt
    add bash prebuilt and custom profile
    
    TODO: build from source with necessary changes to match environment

--- Android.mk
+++ Android.mk
@@ -15,6 +15,16 @@ LOCAL_MODULE_TAGS  := optional
 $(shell mkdir -p $(TARGET_OUT)/xbin)
 $(shell cp -rf $(LOCAL_PATH)/osm0sis_busybox/* $(TARGET_OUT)/xbin/)
 
+# bash prebuilt
+include $(CLEAR_VARS)
+LOCAL_MODULE       := bash_prebuilt
+BASH_EXEC=    := bash
+LOCAL_MODULE_TAGS  := optional
+$(shell mkdir -p $(TARGET_OUT)/xbin)
+$(shell mkdir -p $(TARGET_OUT_ETC)/bash)
+$(shell cp $(LOCAL_PATH)/bash_prebuilt/bash $(TARGET_OUT)/xbin/)
+$(shell cp $(LOCAL_PATH)/bash_prebuilt/bash.bashrc $(TARGET_OUT_ETC)/bash/bashrc)
+
 ifeq ($(TARGET_PRIVATE_BUILD),true)
 
 ## ADD PRIVATE ASSETS HERE NOT TO BE INCLUDED IN PUBLIC BUILDS. DO NOT FORGET to .gitignore!!!
