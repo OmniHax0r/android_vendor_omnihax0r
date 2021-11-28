27777621109786edeb6dccdf456994b3f78a0292
Geofferey Eakins <geofferey.eakins@gmail.com>
2021-11-28 04:51:18 -0800

    add: hid gadget from nethunter
    add hid gadget, controls and sepolicy from nethunter

--- public.mk
+++ public.mk
@@ -4,3 +4,12 @@ TARGET_PUBLIC_BUILD := true
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/omnihax0r
+
+# HID USB Gadget file descriptors 
+PRODUCT_COPY_FILES += \
+	vendor/omnihax0r/hid/keyboard-descriptor.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/keyboard-descriptor.bin \
+	vendor/omnihax0r/hid/mouse-descriptor.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/mouse-descriptor.bin \
+	vendor/omnihax0r/hid/init.hids.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.gadgets.rc
+
+# SELinux Polices
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omnihax0r/sepolicy/private
